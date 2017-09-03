8ea23c6a1d08e6dc08d428389bc2f9df57d4bd27
shiyang0713 <maggie830713@gmail.com>
2017-09-03 13:34:21 +0800

    PTT爬蟲練習

--- src/crawler/example/BasicExample.java
+++ src/crawler/example/BasicExample.java
@@ -2,6 +2,7 @@ package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.select.Elements;
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -20,23 +21,33 @@ public class BasicExample {
 		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
 		// 遠端資料路徑
-		String uri = "http://.../";
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1504404040.A.387.html";
+		//移除所有div顯示文章內容
+		Elements elem =CrawlerPack.start().addCookie("over18","1").getFromHtml(uri)
+						   .select("#main-content");
+				elem.select("div").remove();
 
-		System.out.println(
-				CrawlerPack.start()
-				
+
+
+		System.out.println(elem.text()  //text() 取得tag中的文字內容
+				//CrawlerPack.start()
 				// 參數設定
-			    //.addCookie("key","value")	// 設定cookie
+			    //.addCookie("over18","1")	// 設定cookie
 				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
 				
 				// 選擇資料格式 (三選一)
-				.getFromJson(uri)
+				//.getFromJson(uri)
 			    //.getFromHtml(uri)
 			    //.getFromXml(uri)
 			    
 			    // 這兒開始是 Jsoup Document 物件操作
-			    .select(".css .selector ")
-			    
+
+			    //.select("div.push:contains(推)") //顯示只有推的資料
+				//.select("div.push:contains(推)>.f3.hl.push-userid") //顯示推文id
+				//.select("div.push:contains(推)>.f3.push-content") //顯示推文內容
+
+
+
 		);
 	}
 }
